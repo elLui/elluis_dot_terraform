8924371e89d7b76f47ede6fdaf0c03d9596705a6
luis_cortez <elluis.codes@gmail.com>
2020-12-19 09:26:11 -0600

    added variables.tf - populating instance variables - setting up autoscale group and load blancer

---
+++ main.tf
@@ -0,0 +1,119 @@
+# main terra form file for elluis blog network infrastructure
+# details:
+#     us-east-2
+# @author luis_cortez elluis.codes@gmail.com
+# @version 0.01
+
+
+# prior to running this script through terraform
+# ensure proper aws credentials are supplied
+# developer has local environment setup
+
+terraform {
+  required_version = ">= 0.12 < 0.13"
+}
+
+
+provider "aws" {
+    region = "us-east-2"
+
+    # allows any 2.x version of AWS provider
+    version = "~> 2.0"
+}
+
+
+# set-up default launch server
+
+Root device type: ebs Virtualization type: hvm ENA Enabled: Yes
+resource "aws_launch_configuration" "example" {
+
+  # current ==  ami-0a91cd140a1fc148a
+  # Ubuntu Server 20.04 LTS (HVM),EBS General Purpose (SSD) Volume Type. Support available from Canonical (http://www.ubuntu.com/cloud/services).
+  # free tier eligible
+  image_id = "ami-0a91cd140a1fc148a"
+
+  # Gb == gigabit // GB == gigabyte
+  # t2.micro is free tier eligible
+  # starting at t3 network connectivity goes up to 5Gb
+  # alternative t3.nano(vCPUS = 2, Memory = 0.5GB)
+  # alternative t3.micro(vCPUS = 2, Memory = 1GB)
+  # alternative t3.na(vCPUS = 2, Memory = 2GB)
+  instance_type = "t2.micro"
+
+  # 
+  security_groups = [aws_security_group.instance.id]
+  
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+    lifecycle {
+      create_before_destroy = true
+    }    
+  }
+
+resource "aws_instance" "example" {
+  # image aws ami
+  ami = "ami-0c55b159cbfafe1f0"
+  # t2.micro (free tier)
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+
+  # place the server inside elluis_server security group 
+  vpc_security_group_ids = [ aws_security_group.instance.id ]
+
+
+
+  # this will create and serve index.html file
+  # via built in ubuntu 16 lts server
+
+  # notice that the server busy box commands are wrapped
+  # by nohub commands are -here -yep -ok &
+  # this allows the server to run permanently in
+  # in the background but exit out of the bash script
+
+  # EOF is terraform multi-line comment without having
+  # to insert multiple /n 
+  user_data = <<-EOF
+                #!/bin/bash
+                echo "hello, elluis" > index.html
+                nohub busybox httpd -f -p ${var.server_port}  &
+                EOF
+  tags = {
+    "Name" = "elluis_server"
+  }
+}
+
+
+
+# basic template - 
+# create security group template
+resource "aws_security_group" "instance" {
+  name = "terra-example-instance"
+  ingress {
+      from_port = var.server_port
+      to_port = var.server_port
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+# input variables
+
+variable "server_port" {
+    type = number
+    description = "port server will use for http requests"
+    default = 8080
+}
+
+
+
+
+# output variables
+
+output "public_ip" {
+    value = aws_instance.example.public_ip
+    description = "public ip address of our test server"
+    }
